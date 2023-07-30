c2715bfb72c59893050114e988a34a537485a40f
Liaksej <tarusov.liaksej@gmail.com>
2023-07-31 01:33:39 +0200

    Update package dependencies and add 'licenses.txt'
    Introduced 'copy-webpack-plugin' in 'package.json' for copying individual files or entire directories to the build directory. This is important for moving files that aren't directly imported by our modules. Also added a 'licenses.txt' in the 'src/txt' directory for declaring the licences of website content and images. Expanded 'webpack.common.js' to include a new instance of 'CopyPlugin' to handle the copying of files from 'src/txt'. Updated yarn.lock as per the updated dependencies.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   target: "web",
@@ -44,6 +45,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/txt" }],
     }),
   ],
 };
